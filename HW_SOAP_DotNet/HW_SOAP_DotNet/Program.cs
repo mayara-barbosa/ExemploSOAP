6038a0f3964ee66fcc18ecf00a1e4ef698971248
Mayara Barbosa <maay.bs@hotmail.com>
2016-11-11 20:58:49 -0200

    Add files via upload
    consumidor dotnet

---
+++ HW_SOAP_DotNet/HW_SOAP_DotNet/Program.cs
@@ -0,0 +1,23 @@
+﻿using HW_SOAP_DotNet.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_SOAP_DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            OlaServiceClient hello = new OlaServiceClient();
+            string msg;
+            msg = hello.cumprimentar("Teste");
+            Console.WriteLine(msg);
+            Console.Read();
+
+        }
+    }
+}
